c42bd049d176adce1f5b0e9dd37eb1a5ef3d62a6
Hope Giometti <hopegiometti@gmail.com>
2019-09-27 18:05:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,6 +18,13 @@ def apply_coupons(cart, coupons)
   name = coupon[:item]
 
   if cart[name] && cart[name][:count] >= coupon[:num]
+    if cart["#{name} W/COUPON"][:count] += 1
+    else
+      cart["#{name} W/COUPON"] = {
+        :price => coupon[:cost],
+        :clearance => cart[name][:clearance],
+        :count = 1
+        }
 
 end
 
