b4676db363ac27b9c1311f48a984c894077be1c1
Hope Giometti <hopegiometti@gmail.com>
2019-09-27 18:45:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
         clearance: cart[item][:clearance] ,
         count: coupon[:num]
       }
-      
+      cart[item][:count] -= coupon[:num]
 
 
   #coupons.each do |coupon|
